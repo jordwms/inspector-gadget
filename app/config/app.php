1ccc224e47c31ce0973c2c18819a1cba3895a707
Jordan <profjord@gmail.com>
2013-07-02 13:03:09 -0600

    Installing Lmongo, pre-configuration file.

--- app/config/app.php
+++ app/config/app.php
@@ -113,6 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'LMongo\LMongoServiceProvider',
 
 	),
 
@@ -178,6 +179,8 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'LMongo'          => 'LMongo\Facades\LMongo',
+		'EloquentMongo'   => 'LMongo\Eloquent\Model',
 
 	),
 
